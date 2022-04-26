5427e4e999116b43f993b82a118448ec4e615402
mohamadadithya <mohamadadithya@gmail.com>
2022-04-26 17:02:21 +0700

    add error page with different messages

---
+++ src/components/Error.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let errorTitle, errorMessage;
+</script>
+
+<div>
+	<h1 class="text-3xl mb-3 font-bold">{errorTitle}</h1>
+	<p>{errorMessage}</p>
+</div>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	import Error from '../components/Error.svelte';
+
+	export let status;
+</script>
+
+<section class="grid place-items-center h-screen">
+	{#if status == 500}
+		<Error errorTitle="Something went wrong..." errorMessage="Lobby not found" />
+	{:else}
+		<Error errorTitle="Something went wrong..." errorMessage="Page not found" />
+	{/if}
+</section>
